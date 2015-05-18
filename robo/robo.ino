5c97cc46d1e9c80e97ba06efa0ace0b680f890f3
marcosf63 <marcosf63@gmail.com>
2015-05-18 09:44:12 -0300

    Versao final do rono

--- robo/robo.ino
+++ robo/robo.ino
@@ -14,7 +14,7 @@
 
 
 #define VELOCIDADE_MAXIMA 150
-#define AJUSTE_DA_VELOCIDADE 37
+#define AJUSTE_DA_VELOCIDADE 23
 
 
 
@@ -23,7 +23,7 @@ AF_DCMotor motor1(1, MOTOR12_1KHZ); // create motor #1 using M1 output on Motor 
 AF_DCMotor motor2(4, MOTOR12_1KHZ); // create motor #2, using M2 output, set to 1kHz PWM frequency
 
 Servo servo1, servo2;
-int ch1, ch2, ch3, ch4, x, y;
+int ch6, ch2, ch3, ch4, ch5, x, y;
 int velocidade_motor;
 
 
@@ -42,11 +42,15 @@ void para();                // para o robot
 void setup() {
   pinMode(7, INPUT);
   pinMode(6, INPUT);
+  pinMode(14, INPUT);
+  pinMode(15, INPUT);
   Serial.begin(9600);
   para();
   servo1.attach(10);
+  servo1.write(0);
   servo2.attach(9);
-  servo2.write(0);
+  servo2.write(90);
+  
   
   
   
@@ -57,53 +61,53 @@ void setup() {
 void loop() {
    ch3 = pulseIn(7, HIGH, 25000);
    ch4 = pulseIn(6, HIGH, 25000);
- 
    
-   
-   while (ch4 >= 1500 && ch4 < 1600) {
-      ch1 = pulseIn(14, HIGH, 25000);
-      ch2 = pulseIn(15, HIGH, 25000);
-      ch3 = pulseIn(7, HIGH, 25000);
-      ch4 = pulseIn(6, HIGH, 25000);
-      if (ch3 >= 1650) {
-        move_para_frente();
-        
-      } else {
-        if (ch3 < 1450){
-          move_para_tras();
-        } else {
-          para();
-        }
-      }
-      //delay(3000);
-      //controle da garra
-      servo1.write(map(ch1, 1100, 1900, 0,90));
-      servo1.write(map(ch2, 1100, 1900, 0,90));
       
-      
-      
-   }
-   
-   if (ch4 < 1500) {
-     vira_para_esquerda();
-     //delay(3000);
-   } else {
-     if (ch4 >= 1600)
+   if (ch3 != 0) {
+     
+     while(ch4 > 1400 && ch4 < 1800) {
+       ch3 = pulseIn(7, HIGH, 25000);
+       ch4 = pulseIn(6, HIGH, 25000);
+       ch5 = pulseIn(15, HIGH, 25000);
+       ch6 = pulseIn(14, HIGH, 25000);
+       //canal 2 variando de 1280 a 1900
+       
+       if (ch3 < 1450)
+         move_para_tras();
+       else {
+         if (ch3 > 1650)
+           move_para_frente();
+         else
+           para();
+           Serial.println(ch5);
+           //delay(2000);
+           if (ch5 > 1500)
+             servo2.write(90);
+             while (ch5 > 1500)
+               servo2.write(map(ch6, 1000, 2000, 90, 180));
+           else
+             servo1.write(0);
+             while (ch5 < 1500)
+               servo1.write(map(ch6, 1000, 2000, 0, 180));
+       }
+     }
+     
+     if (ch4 <= 1400)
+       vira_para_esquerda();
+     else
        vira_para_direita();
-       //delay(3000);
+     
+     
+     
+     
+   } else {
+     
+       para();
+     
    }
+}
    
 
- 
- 
- 
-
- 
- 
- 
-  
- 
-}
 //-------------------------------------------------------------------------------------------------------------------------------------
 
 
@@ -113,7 +117,7 @@ void move_para_frente(){
   motor2.run(FORWARD);
   for (velocidade_motor = 0; velocidade_motor < VELOCIDADE_MAXIMA; velocidade_motor +=2){
     motor1.setSpeed(velocidade_motor);
-    motor2.setSpeed(velocidade_motor);
+    motor2.setSpeed(velocidade_motor + AJUSTE_DA_VELOCIDADE);
   
   }
   
@@ -124,23 +128,34 @@ void move_para_tras(){
   motor2.run(BACKWARD);
   for (velocidade_motor = 0; velocidade_motor < VELOCIDADE_MAXIMA; velocidade_motor +=2){
     motor1.setSpeed(velocidade_motor);
-    motor2.setSpeed(velocidade_motor);
+    motor2.setSpeed(velocidade_motor + AJUSTE_DA_VELOCIDADE);
   
   }
   
 }
 
+void imprime_canais(){
+   Serial.print("ch3: ");
+   Serial.print(ch3);
+  
+   
+   Serial.print("  ch4: ");
+   Serial.println(ch4);
+   delay(2000);
+   
+
+}
 
 void vira_para_esquerda(){
   motor1.run(FORWARD);
-  motor2.run(BACKWARD);
+  motor2.run(RELEASE);
   delay(200);
   
   
 }
 
 void vira_para_direita(){
-  motor1.run(BACKWARD);
+  motor1.run(RELEASE);
   motor2.run(FORWARD);
   delay(200);
   
